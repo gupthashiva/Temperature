fb6c5787a42eba4edce6f98b843b0b73fe0d777a
gupthashiva <144518221+gupthashiva@users.noreply.github.com>
2023-09-09 12:44:27 +0530

    Add files via upload

---
+++ temperature.js
@@ -0,0 +1,18 @@
+const celsiusInput = document.getElementById('celsius');
+const fahrenheitInput = document.getElementById('fahrenheit');
+
+celsiusInput.addEventListener('input', () => {
+    const celsiusValue = parseFloat(celsiusInput.value);
+    if (!isNaN(celsiusValue)) {
+        const fahrenheitValue = celsiusValue * 9 / 5 + 32;
+        fahrenheitInput.value = fahrenheitValue.toFixed(2);
+    }
+});
+
+fahrenheitInput.addEventListener('input', () => {
+    const fahrenheitValue = parseFloat(fahrenheitInput.value);
+    if (!isNaN(fahrenheitValue)) {
+        const celsiusValue = (fahrenheitValue - 32) * 5 / 9;
+        celsiusInput.value = celsiusValue.toFixed(2);
+    }
+});
